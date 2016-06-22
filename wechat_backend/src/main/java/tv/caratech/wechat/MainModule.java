fbcae88dfed2a44d0b4d367d0427ce0c046954b0
sgt39007 <sgt39007@163.com>
2016-06-22 17:37:29 +0800

    start

--- wechat_backend/src/main/java/tv/caratech/wechat/MainModule.java
+++ wechat_backend/src/main/java/tv/caratech/wechat/MainModule.java
@@ -2,16 +2,10 @@ package tv.caratech.wechat;
 
 import org.nutz.mvc.annotation.IocBy;
 import org.nutz.mvc.annotation.Modules;
+import org.nutz.mvc.annotation.SetupBy;
 import org.nutz.mvc.ioc.provider.ComboIocProvider;
 
-/*
-一个 Mvc 框架可以通过 Ioc接口 同一个 Ioc容器 挂接，挂接的方法很简单： 在主模块上声明 @IocBy
-
-ComboIocProvider的args参数, 星号开头的是类名或内置缩写,剩余的是各加载器的参数
-*js 是JsonIocLoader,负责加载js/json结尾的ioc配置文件
-*anno 是AnnotationIocLoader,负责处理注解式Ioc, 例如@IocBean
-*tx 是TransIocLoader,负责加载内置的事务拦截器定义, 1.b.52开始自带
-*/
+@SetupBy(value = MainSetup.class)
 @IocBy(type=ComboIocProvider.class, args={"*js", "ioc/",
         "*anno", "cn.jdworks.wechat",
         "*tx"})
---
+++ wechat_backend/src/main/java/tv/caratech/wechat/MainSetup.java
@@ -0,0 +1,32 @@
+package tv.caratech.wechat;
+
+import org.nutz.dao.Dao;
+import org.nutz.dao.util.Daos;
+import org.nutz.ioc.Ioc;
+import org.nutz.mvc.NutConfig;
+import org.nutz.mvc.Setup;
+
+import tv.caratech.wechat.bean.User;
+
+public class MainSetup implements Setup{
+
+	public void init(NutConfig nc) {
+		Ioc ioc = nc.getIoc();
+		Dao dao = ioc.get(Dao.class);
+		Daos.createTablesInPackage(dao, "tv.caratech.wechat.bean", true);
+		
+		//初始化根用户目录
+		if (dao.count(User.class) == 0) {
+			User user = new User();
+			user.setName("admin");
+            user.setPassword("123456");
+            
+            dao.insert(user);
+		}
+	}
+	
+	public void destroy(NutConfig nc) {
+		// TODO Auto-generated method stub
+		
+	}
+}
