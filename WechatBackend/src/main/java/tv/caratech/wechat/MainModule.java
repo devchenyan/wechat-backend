288bfa9cdcb320c46191eda23fe1e9926b925ae5
sgt39007 <sgt39007@163.com>
2016-06-23 09:28:57 +0800

    user

--- WechatBackend/src/main/java/tv/caratech/wechat/MainModule.java
+++ WechatBackend/src/main/java/tv/caratech/wechat/MainModule.java
@@ -1,6 +1,15 @@
 package tv.caratech.wechat;
 
+import org.nutz.mvc.annotation.IocBy;
 import org.nutz.mvc.annotation.Modules;
+import org.nutz.mvc.annotation.SetupBy;
+import org.nutz.mvc.ioc.provider.ComboIocProvider;
+
+
+@SetupBy(value=MainSetup.class)
+@IocBy(type=ComboIocProvider.class, args={"*js", "ioc/",
+        "*anno", "tv.caratech.wechat",
+        "*tx"})
 
 @Modules(scanPackage=true)
 public class MainModule {
